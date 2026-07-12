51e9bf77e50b8657e0fedb4261e241be4bc3716b
zibbble glitch <zibbbleglitch@gmail.com>
2026-07-12 12:29:58 -0400

    try something else

--- home.nix
+++ home.nix
@@ -65,8 +65,9 @@ in
 
   programs.alacritty = {
     enable = true;
-    theme = pkgs.alacritty-theme.cyber_punk_neon;
+    # theme = pkgs.alacritty-theme.cyber_punk_neon;
     # theme = "cyber_punk_neon";
+    theme = dracula;
     # settings.general.import = [ pkgs.alacritty-theme.cyber_punk_neon ];
   };
 
